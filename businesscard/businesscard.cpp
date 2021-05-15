d0d83083fdf61ad937eb145edf9bfed8510b91fd
QuinCarr <81778210+QuinCarr@users.noreply.github.com>
2021-05-14 19:26:18 -0500

    Add files via upload

---
+++ businesscard/businesscard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Kings Bowl Chinese Restaurant" << std::endl; 
+    std::cout << "5727 Babcock Rd, San Antonio, TX 78240" << std::endl;
+    std::cout << "210-690-8819" << std::endl;
+    std::cout << "https://kingsbowlchinesefood.com/" << std::endl; 
+    return 0;
+}
